f0b2f20dd8c604aba1bcad7f64efd36e727d4258
wzhorton <wz.horton@gmail.com>
2018-12-27 12:51:48 -0700

    package setup

---
+++ compile_pkg.R
@@ -0,0 +1,18 @@
+#### compile_pkg.R ####
+# This file is meant for recompilation during development.
+# For regular installation use devtools::install_github("user/pkgname")
+# Use build_vignettes = TRUE in install_github to get html vignettes
+
+library(roxygen2)
+library(devtools)
+
+rm(list = ls()) # cleans current environment
+gc() # releases memory removed above
+
+# create NAMESPACE and .Rd files for documentation. Compile C files if any.
+devtools::document()
+
+# install preliminary package for function testing and documentation inspection
+# setting build_vignettes = FALSE will considerably speed up installation.
+# if built with vignettes, use browseVignettes("pkgname") to view.
+devtools::install(build_vignettes = FALSE)
---
+++ src/cpp_functions.cpp
@@ -0,0 +1,5 @@
+#include <RcppArmadillo.h>
+using namespace Rcpp;
+
+// [[Rcpp::depends(RcppArmadillo)]]
+
